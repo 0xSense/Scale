ea6215fab53f2b42ab0d81adc2dad7898aa42a15
alexandervenezia <58950620+alexandervenezia@users.noreply.github.com>
2023-11-01 22:46:15 -0400

    feat: completed minimal implementation of CardData.cs, Deck.cs, MasterDeck.cs

--- systems/combat/Deck.cs
+++ systems/combat/Deck.cs
@@ -18,6 +18,27 @@ public class Deck
 
     public void AddCard(CardData card)
     {
-        
+        if (_cards.ContainsKey(card))
+            _cards[card]++;
+        else
+            _cards.Add(card, 1);
     }
+
+    public void RemoveCard(CardData card)
+    {
+        if (_cards.ContainsKey(card))
+        {
+            _cards[card]--;
+            if (_cards[card] <= 0)
+                _cards.Remove(card);
+        }
+    }
+
+    public int GetCount(CardData card)
+    {
+        if (!_cards.ContainsKey(card))
+            return 0;
+        return _cards[card];
+    }
+
 }
--- systems/combat/MasterDeck.cs
+++
@@ -1,9 +0,0 @@
-// This class manages all card types in the game
-namespace Systems.Combat;
-
-using Godot;
-
-public partial class MasterDeck : Node2D
-{
-
-}
