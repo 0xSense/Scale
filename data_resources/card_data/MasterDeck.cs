4c9f4af6b76e2fd6689b24c204531a69e3324219
alexandervenezia <58950620+alexandervenezia@users.noreply.github.com>
2023-11-01 23:07:48 -0400

    fix: fixed indentation

--- data_resources/card_data/MasterDeck.cs
+++ data_resources/card_data/MasterDeck.cs
@@ -6,7 +6,7 @@ using Data;
 using Godot;
 
 public partial class MasterDeck : Node2D
-{
+{	
 	[Export] private Godot.Collections.Array<CardData> _cardTypes = new();
 	public static Godot.Collections.Array<CardData> CardTypes = new();
 
