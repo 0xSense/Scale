c2f410768944fe10d152352b663cd9e20d28cbdf
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-02 14:05:34 +0100

    Polish movement
    Not working

--- scenes/overworld/PlayerMovement.cs
+++ scenes/overworld/PlayerMovement.cs
@@ -3,8 +3,11 @@ using System;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float maxSpeed = 300.0f;
+	public float currentSpeed;
+	public float speedDiff;
+	public const float jumpVelocity = -400.0f;
+	public const float accelerationRate = 0.8f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -19,18 +22,20 @@ public partial class PlayerMovement : CharacterBody2D
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+			velocity.Y = jumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			speedDiff  = maxSpeed - Velocity.X;
+			currentSpeed = speedDiff * accelerationRate;
+			velocity.X = direction.X * currentSpeed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, currentSpeed);
 		}
 
 		Velocity = velocity;
