682f7be76b893d8b97620e358cd9a3c47a51c340
alexandervenezia <58950620+alexandervenezia@users.noreply.github.com>
2023-11-02 22:07:10 -0400

    feat: added ability to get cards from deck

--- systems/combat/Deck.cs
+++ systems/combat/Deck.cs
@@ -10,7 +10,9 @@ using Data;
 public class Deck
 {
     private Dictionary<CardData, int> _cards; // Maps cards to number of instances in this deck
-    
+    public Dictionary<CardData, int> CardDict => _cards;
+
+
     public Deck()
     {
         _cards = new();
@@ -39,6 +41,41 @@ public class Deck
         if (!_cards.ContainsKey(card))
             return 0;
         return _cards[card];
+    }
+
+    /* Returns all cards */
+    public List<CardData> GetCards()
+    {
+        return GetCards((CardData c) => true);
+    }
+
+
+    /*
+    Generic filtered card getter. Example to get all action cards with an action point cost greater than 2:
+
+    GetCards(
+        (CardData c) => {
+            return c.Type == CardType.ACTION && c.ActionPointCost > 2
+        }
+
+    )
+
+    Filter(CardData) -> bool; returns true if card is to be included in return, else otherwise.
+    */
+    public List<CardData> GetCards(System.Func<CardData, bool> filter)
+    {
+        List<CardData> cards = new();
+
+        foreach (CardData c in _cards.Keys)
+        {
+            for (int i = 0; i < _cards[c]; i++)
+            {
+                if (filter(c))
+                    cards.Add(c);
+            }
+        }
+
+        return cards;
     }
 
 }
