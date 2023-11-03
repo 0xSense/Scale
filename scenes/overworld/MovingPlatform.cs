bd26aacfe7a7a51f092fb2a1ff7c550add67eec1
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-03 17:09:22 +0100

    feat: Add Moving Platforms

---
+++ scenes/overworld/MovingPlatform.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class MovingPlatform : StaticBody2D
+{
+	[Export] private float _movingSpeed;
+	[Export] private bool _X;
+	[Export] private bool _Y;
+	[Export] private float _movingDistance;
+	private Vector2 _startingPosition; 
+
+    public override void _Ready()
+    {
+        _startingPosition = Position;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+		if (_X)
+		{
+			if (_startingPosition.X + _movingDistance >= Position.X)
+			{
+				Position += new Vector2(_movingSpeed, 0) * (float)delta;
+			} 
+			else
+			{
+				GD.Print(_startingPosition.X);
+				GD.Print(Position.X);
+			}
+		} 
+		else if (_Y)
+		{
+			if (_startingPosition.Y + _movingDistance >= Position.Y)
+			{
+				Position += new Vector2(0, _movingSpeed) * (float)delta;
+			} 
+		}
+		
+		
+
+		
+		
+		
+	}
+}
