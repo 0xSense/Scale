69e82c464a86738ae785e881128f9ac681117a79
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-04 14:07:18 +0100

    fix: Enemy follows Player if in reach

--- scenes/overworld/Pathfinding.cs
+++ scenes/overworld/Pathfinding.cs
@@ -1,21 +1,23 @@
 using Godot;
 using System;
+using System.IO;
 
 public partial class Pathfinding : Node2D
 {
 	[Export] private float _speed;
-	[Export] private float _acceleration;
 	private Vector2 _direction;
 	private NavigationAgent2D _navAgent;
 	private Path2D _path;
 	private CharacterBody2D _enemyBody;
 	private bool _playerInRange;
+	private CharacterBody2D _playerBody;
 
     public override void _Ready()
     {
         _navAgent = GetNode<NavigationAgent2D>("./Enemy_Body/NavigationAgent2D");
-		_path = GetNode<Path2D>("./Path2D");
+//		_path = GetNode<Path2D>("./Path2D");
 		_enemyBody = GetNode<CharacterBody2D>("./Enemy_Body");
+		_playerBody = GetNode<CharacterBody2D>("../Player");
     }
 
     public override void _Process(double delta)
@@ -23,15 +25,16 @@ public partial class Pathfinding : Node2D
 		if (_playerInRange)
 		{
 			_direction = new Vector2();
-			
-			_navAgent.TargetPosition = GetGlobalMousePosition();
 
-			_direction = _navAgent.GetNextPathPosition() - GlobalPosition;
+			_navAgent.TargetPosition = _playerBody.GlobalPosition;
+			GD.Print(GlobalPosition);
+			_direction = _navAgent.GetNextPathPosition() - _enemyBody.GlobalPosition;
 			_direction = _direction.Normalized();
 
-			_enemyBody.Velocity = _enemyBody.Velocity.Lerp(_direction * _speed, _acceleration * (float)delta);
+			_enemyBody.Velocity = _direction * _speed;
+
+			_enemyBody.MoveAndSlide();
 		}
-        _enemyBody.MoveAndSlide();
 	}
 
 	public void _on_area_2d_body_entered(Node2D body) 
@@ -42,7 +45,5 @@ public partial class Pathfinding : Node2D
 	public void _on_area_2d_body_exited(Node2D body) 
 	{
 		_playerInRange = false;
-		GD.Print(body);
-		GD.Print("Left Zone");
 	}
 }
