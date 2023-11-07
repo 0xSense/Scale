220649c3cfbd7b966cc443dc6aea39410dc8666c
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-07 22:16:07 +0100

    feat: add working doors

---
+++ scenes/overworld/door.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class door : Node2D
+{
+	[Export] private Marker2D _targetLocation;
+	[Export] private CharacterBody2D _player;
+	private bool _playerInsideRange;
+
+	public override void _Process(double delta)
+	{
+		if (_playerInsideRange)
+		{
+			if(Input.IsActionJustReleased("ui_interact"))
+			{
+				_player.Position = _targetLocation.Position;
+			}
+		}
+	}
+
+	public void OnAreaEntered(Node2D body)
+	{
+		_playerInsideRange = true;
+	}
+
+	public void OnAreaExited(Node2D body)
+	{
+		_playerInsideRange = false;
+	}
+}
