5c2838c4741e3c41fbe2f55298886066ed0f211c
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-08 14:19:08 +0100

    fix: try to parse json files
    not working

--- scenes/overworld/npcs/npc.cs
+++ scenes/overworld/npcs/npc.cs
@@ -1,14 +1,19 @@
 using Godot;
 using System;
+using System.IO;
 
 public partial class npc : Node
 {
 	[Export] private CharacterBody2D _playerBody;
+	[Export] private string _dialogueFile;
 	private bool _playerInRange;
+	private NinePatchRect _dialogue;
+	private FileStream _json = File.Open("res://Resources/missions.json", FileMode.Open);
 
 	public override void _Ready()
 	{
-
+		_dialogue = GetNode<NinePatchRect>("./Dialogue");
+		var text = _json.Read();
 	}
 
 	public override void _Process(double delta)
