6a251f6511bade0f04a54870d611058a6502f145
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-09 10:55:57 +0100

    feat: continue npcs

--- scenes/overworld/npcs/npc.cs
+++ scenes/overworld/npcs/npc.cs
@@ -1,19 +1,28 @@
 using Godot;
+using Godot.Collections;
+using Microsoft.VisualBasic;
 using System;
 using System.IO;
+using System.Text.Json.Nodes;
 
 public partial class npc : Node
 {
 	[Export] private CharacterBody2D _playerBody;
-	[Export] private string _dialogueFile;
+	[Export] private string _dialogueFilePath;
+	[Export] private RichTextLabel _nameField;
+	[Export] private RichTextLabel _textField;
 	private bool _playerInRange;
 	private NinePatchRect _dialogue;
-	private FileStream _json = File.Open("res://Resources/missions.json", FileMode.Open);
+	private string _json; 
+	private Godot.Collections.Dictionary _dialogueText = new Godot.Collections.Dictionary();
 
 	public override void _Ready()
 	{
 		_dialogue = GetNode<NinePatchRect>("./Dialogue");
-		var text = _json.Read();
+		_json = Godot.FileAccess.Open(_dialogueFilePath, Godot.FileAccess.ModeFlags.Read).GetAsText();
+		var jsonFile = Godot.(_json);
+		Dictionary<object, object> Parsed = jsonFile as Dictionary;
+		
 	}
 
 	public override void _Process(double delta)
@@ -22,7 +31,7 @@ public partial class npc : Node
 		{
 			if (Input.IsActionJustPressed("ui_interact"))
 			{
-				
+				GD.Print(_dialogueText["d"]);
 			}
 		}
 	}
