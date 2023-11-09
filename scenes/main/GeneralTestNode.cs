71294eead4be0be40d751574aa72a0efb11a8783
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-09 17:19:05 +0100

    Revert "Revert "Merge branch 'main' into feature/overworld""
    This reverts commit 045f25a026474295007b2f67eee4cbe661c25738.

--- scenes/main/GeneralTestNode.cs
+++ scenes/main/GeneralTestNode.cs
@@ -9,8 +9,5 @@ public partial class GeneralTestNode : Node2D
     public override void _Ready()
     {
         Deck deck = new Deck();
-        CardData card = new();
-        CardData card2 = new();
-
     }
 }
---
+++ scenes/purgatory/scenes/door.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+using System.IO;
+
+public partial class door : Area2D
+{
+	private bool _doorEntered;
+	[Export] private string _sceneTransition;
+
+	public void _on_body_entered(Node2D body)
+	{
+		GD.Print("Hello cutie");
+		_doorEntered = true;
+	}
+	public void _on_body_exited(Node2D body)
+	{
+		GD.Print("Goodbye cutie :(");
+		_doorEntered = false;
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (_doorEntered)
+		{
+			if (Input.IsActionJustReleased("ui_interact"))
+			{
+				GetTree().ChangeSceneToFile(_sceneTransition);
+
+			}
+		}
+	}
+}
