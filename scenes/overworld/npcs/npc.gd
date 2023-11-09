3c11099aa58a0ba2054a08fea97df6c244ecbe88
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2023-11-09 16:03:19 +0100

    feat: add shop and some other stuff forgot

--- scenes/overworld/npcs/npc.gd
+++ scenes/overworld/npcs/npc.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var player: CharacterBody2D
+@export var shop: Node2D
 @export var json_file_path:String
 var player_in_range: bool
 var json_as_dict
@@ -12,18 +13,13 @@ func _ready():
 	json_as_dict = JSON.parse_string(json_as_text)
 	npc_name = get_node(".").name
 	$Dialogue.visible = false
+	$Dialogue/Options.visible = false
 	$Dialogue/Name.text = npc_name
 
 func _process(_delta):
 	if player_in_range:
 		if Input.is_action_just_pressed("ui_interact"):
-			$Dialogue.visible = true
-			if !(index == len(json_as_dict[npc_name][index]) - 1) :	
-				$Dialogue/Text.text = json_as_dict[npc_name][index]["text"]
-				index += 1
-			else:
-				$Dialogue/Text.text = json_as_dict[npc_name][index]["text"]
-				index = 0
+			_get_text()
 
 func _on_area_2d_body_entered(body):
 	if body == player:
@@ -32,3 +28,32 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body == player:
 		player_in_range = false
+
+func _get_text():
+	$Dialogue/Text.text = json_as_dict[npc_name][index]["text"]
+	$Dialogue.visible = true
+	if !(index == len(json_as_dict[npc_name]) - 1) :	
+		if !(len(json_as_dict[npc_name][index]["options"]) == 0):
+			$Dialogue/Options/Button_1.text = json_as_dict[npc_name][index]["options"][0]
+			$Dialogue/Options/Button_2.text = json_as_dict[npc_name][index]["options"][1]
+			$Dialogue/Options.visible = true
+		else:
+			$Dialogue/Options.visible = false
+		index += 1
+	else:
+		if !(len(json_as_dict[npc_name][index]["options"]) == 0):
+			$Dialogue/Options/Button_1.text = json_as_dict[npc_name][index]["options"][0]
+			$Dialogue/Options/Button_2.text = json_as_dict[npc_name][index]["options"][1]
+			$Dialogue/Options.visible = true
+		else:
+			$Dialogue/Options.visible = false
+		index = 0
+	
+func _on_button_1_pressed():
+	if len(json_as_dict[npc_name][index - 1]["options"]) == 3:
+		if json_as_dict[npc_name][index - 1]["options"][2] == "shop":
+			shop.visible = true
+	_get_text()
+			
+func _on_button_2_pressed():
+	_get_text()
---
+++ scenes/overworld/shop.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class shop : Node2D
+{
+	public void ButtonItem(int i)
+	{
+		GD.Print("Give Player Item " + i);
+		GetChild<GridContainer>(1).GetChild<Panel>(i-1).Visible = false;
+	}
+
+	public void OnClosePressed()
+	{
+		this.Visible = false;
+	}
+}
