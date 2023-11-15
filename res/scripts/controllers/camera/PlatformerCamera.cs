a89b1b31220c76e02137df31a09bdcbb086ef951
Sense <105939929+0xSense@users.noreply.github.com>
2023-11-15 13:12:46 -0500

    feat: 10 frames added to character movement

--- res/scripts/controllers/camera/PlatformerCamera.cs
+++ res/scripts/controllers/camera/PlatformerCamera.cs
@@ -6,7 +6,7 @@ public partial class PlatformerCamera : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Node2D.
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
