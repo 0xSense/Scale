44e4c6d4e34ddebc7add0c3f74b2df6f1492005d
Sense <105939929+0xSense@users.noreply.github.com>
2023-11-16 20:05:43 -0500

    chore: Node<animated_sprite_2D> added to shopkeeper scene

--- res/scripts/managers/purgatory/ShopKeeperRoom.cs
+++ res/scripts/managers/purgatory/ShopKeeperRoom.cs
@@ -6,7 +6,7 @@ public partial class ShopKeeperRoom : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		GD.Print(GetChild<Marker2D>(0));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
